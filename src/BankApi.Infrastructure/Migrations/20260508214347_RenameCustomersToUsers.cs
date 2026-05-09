d7d8c2c20d9dd23e1bcc4ff85c1f7b53589dff7a
suporte05 <suporte05@atsditsolutions.com>
2026-05-09 11:37:34 -0300

    fix: changing migration history for new table Users

---
+++ src/BankApi.Infrastructure/Migrations/20260508214347_RenameCustomersToUsers.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankApi.Infrastructure.Migrations
+{
+    public partial class RenameCustomersToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                DO $$
+                BEGIN
+                    IF EXISTS (
+                        SELECT 1
+                        FROM information_schema.tables
+                        WHERE table_schema = 'public'
+                          AND table_name = 'Customers'
+                    ) THEN
+                        ALTER TABLE "Customers" RENAME TO "Users";
+                    END IF;
+                END
+                $$;
+                """);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                DO $$
+                BEGIN
+                    IF EXISTS (
+                        SELECT 1
+                        FROM information_schema.tables
+                        WHERE table_schema = 'public'
+                          AND table_name = 'Users'
+                    ) THEN
+                        ALTER TABLE "Users" RENAME TO "Customers";
+                    END IF;
+                END
+                $$;
+                """);
+        }
+    }
+}
